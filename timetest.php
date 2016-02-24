a4f0dad22ac0a0833b76832b8a04de0ebaf91347
bmesh <bryanmeshke@yahoo.com>
2016-02-23 16:22:14 -0900

    Update timetest.php

--- timetest.php
+++ timetest.php
@@ -1,6 +1,27 @@
+
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>index this</title>
+<style>
+	body {
+		background-color: #6FF	
+	}
+</style>
+</head>
+
+<body>
+<p>first up starts here</p>
+<p></p><a href="initial.html">link to initial</a></p>
+<p><a href="whatabout.html">what about this page?</a></p>
 <?php
 
 // Prints something like: Monday
 echo date("l");
 
 ?>
+</body>
+</html>
